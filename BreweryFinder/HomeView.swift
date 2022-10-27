1ae03f34cb3a65ed86afe96ead83f34dcbd273bf
Al Wold <alwold@alwold.com>
2022-10-26 20:15:53 -0700

    Basic view for home

---
+++ BreweryFinder/HomeView.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+final class HomeView: UIView {
+    let titleLabel = UILabel()
+    
+    init() {
+        super.init(frame: .zero)
+        addSubview(titleLabel)
+        
+        setConstraints()
+
+        titleLabel.text = "Brewery Finder"
+    }
+        
+    private func setConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 64)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+#if DEBUG
+import SwiftUI
+
+struct HomeView_Preview: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            HomeView()
+        }
+    }
+}
+#endif
