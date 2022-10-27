6c41c26e32ab1ddb2fa76ccc7ec6c35edd796a8b
Al Wold <alwold@alwold.com>
2022-10-26 21:01:38 -0700

    A basic layout for the home view

--- BreweryFinder/HomeView.swift
+++ BreweryFinder/HomeView.swift
@@ -1,23 +1,48 @@
 import UIKit
 
 final class HomeView: UIView {
+    let backgroundImage = UIImageView(image: UIImage(named: "background"))
+    
     let titleLabel = UILabel()
+    
+    let findByLocationButton = UIButton(type: .roundedRect, primaryAction: UIAction { _ in
+        print("tap")
+    })
     
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+        backgroundImage.contentMode = .scaleAspectFill
+        addSubview(backgroundImage)
         addSubview(titleLabel)
+        addSubview(findByLocationButton)
         
         setConstraints()
 
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
         titleLabel.text = "Brewery Finder"
+        titleLabel.textColor = .orange
     }
         
     private func setConstraints() {
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        findByLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        findByLocationButton.setImage(UIImage(systemName: "location.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 64, weight: .bold, scale: .large)), for: .normal)
+        findByLocationButton.tintColor = .orange
+
         NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            backgroundImage.leftAnchor.constraint(equalTo: leftAnchor),
+            backgroundImage.rightAnchor.constraint(equalTo: rightAnchor),
+            
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 64)
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 64),
+            
+            findByLocationButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            findByLocationButton.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
     }
     
