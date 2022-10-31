57be5e111f87fe6f3c9a9cb259519ce6c87cc4f3
Al Wold <alwold@alwold.com>
2022-10-30 21:32:57 -0700

    Add some more stuff to the brewery tile

--- BreweryFinder/Nearby Breweries/NearbyBreweryCell.swift
+++ BreweryFinder/Nearby Breweries/NearbyBreweryCell.swift
@@ -1,13 +1,21 @@
 import UIKit
 
 final class NearbyBreweryCell: UITableViewCell {
+    let iconView = UIImageView(image: UIImage(systemName: "wineglass", withConfiguration: UIImage.SymbolConfiguration(pointSize: 24, weight: .regular, scale: .large)))
     let boxView = UIView()
     let nameLabel = UILabel()
-    
+    let typeLabel = UILabel()
+    let chevron = UIImageView(image: UIImage(systemName: "chevron.right", withConfiguration: UIImage.SymbolConfiguration(pointSize: 24, weight: .regular, scale: .large)))
+    let distanceLabel = UILabel()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(boxView)
+        boxView.addSubview(iconView)
         boxView.addSubview(nameLabel)
+        boxView.addSubview(typeLabel)
+        boxView.addSubview(distanceLabel)
+        boxView.addSubview(chevron)
         
         setConstraints()
 
@@ -17,16 +25,28 @@ final class NearbyBreweryCell: UITableViewCell {
         boxView.layer.shadowRadius = 2
         boxView.layer.shadowOpacity = 1
         boxView.layer.cornerRadius = 8
+        
+        iconView.tintColor = .white
                 
         nameLabel.font = .cardTitle
         nameLabel.numberOfLines = 0
         nameLabel.lineBreakMode = .byWordWrapping
         nameLabel.textColor = .white
+        
+        typeLabel.textColor = .white
+        
+        distanceLabel.textColor = .white
+        
+        chevron.tintColor = .white
     }
     
     private func setConstraints() {
         boxView.translatesAutoresizingMaskIntoConstraints = false
+        iconView.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        distanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        chevron.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             boxView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
@@ -34,10 +54,22 @@ final class NearbyBreweryCell: UITableViewCell {
             boxView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
             boxView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
             
-            nameLabel.centerXAnchor.constraint(equalTo: boxView.centerXAnchor),
-            nameLabel.centerYAnchor.constraint(equalTo: boxView.centerYAnchor),
-            nameLabel.topAnchor.constraint(equalTo: boxView.topAnchor, constant: 30),
-            nameLabel.leadingAnchor.constraint(equalTo: boxView.leadingAnchor, constant: 16)
+            iconView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            iconView.leadingAnchor.constraint(equalTo: boxView.leadingAnchor, constant: 16),
+            
+            nameLabel.topAnchor.constraint(equalTo: boxView.topAnchor, constant: 16),
+            nameLabel.leadingAnchor.constraint(equalTo: iconView.trailingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: chevron.leadingAnchor, constant: -16),
+            
+            typeLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            typeLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor),
+            
+            distanceLabel.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 8),
+            distanceLabel.leadingAnchor.constraint(equalTo: typeLabel.leadingAnchor),
+            distanceLabel.bottomAnchor.constraint(equalTo: boxView.bottomAnchor, constant: -16),
+            
+            chevron.centerYAnchor.constraint(equalTo: boxView.centerYAnchor),
+            chevron.trailingAnchor.constraint(equalTo: boxView.trailingAnchor, constant: -16)
         ])
     }
     
@@ -54,6 +86,8 @@ struct NearbyBreweryCell_Preview: PreviewProvider {
         UIViewPreview {
             let cell = NearbyBreweryCell(style: .default, reuseIdentifier: nil)
             cell.nameLabel.text = "Four Peaks Brewing Co and stuff like that"
+            cell.typeLabel.text = "Microbrewery"
+            cell.distanceLabel.text = "4.2 miles away"
             return cell
         }
         .frame(width: 360, height: 200)
